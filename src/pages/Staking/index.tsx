6149074f7fab8eed6f6eeff7518bf1d18e2583c6
koufeng <feng.kou@hfin.io>
2023-02-01 14:20:47 +0800

    add icon-font

--- src/pages/Staking/index.tsx
+++ src/pages/Staking/index.tsx
@@ -1,34 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
 import { AutoColumn } from '../../components/Column'
-import { useActiveWeb3React } from '../../hooks'
+// import { useActiveWeb3React } from '../../hooks'
 // import { useTokenBalance } from '../../state/wallet/hooks'
 import { USDT } from '../../constants'
 
 import './index.scss'
 
-import { Button } from 'antd'
 const PageWrapper = styled(AutoColumn)`
-  max-width: 640px;
+  max-width: 1280px;
   width: 100%;
 `
 
 export default function Staking() {
-  const { account, chainId } = useActiveWeb3React()
+  // const { account, chainId } = useActiveWeb3React()
   console.log(USDT)
   // const usdtBalance = useTokenBalance(account ?? undefined, USDT)
   // console.log(usdtBalance)
   // const usdtBalance = useTokenBalance(account ?? undefined, USDT[chainId ?? 56])
-  const bal = 0
   return (
     <>
       <PageWrapper>
         <div className="staking-page">
-          staking{bal} {account}
+          <i className="iconfont">&#xe605;</i>
+          <i className="iconfont">&#xe606;</i>
+          <i className="iconfont">&#xe607;</i>
         </div>
-        <Button>Default Button</Button>
-        <div className="text-success">{chainId}</div>
-        {/* <div>{usdtBalance?.toFixed()}</div> */}
       </PageWrapper>
     </>
   )
