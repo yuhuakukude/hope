864d79c8d02a82a790023e29261b99686ef95dd0
koufeng <feng.kou@hfin.io>
2023-02-07 00:36:38 +0800

    gomboc vote init

--- src/pages/DaoGomboc/components/Vote/index.tsx
+++ src/pages/DaoGomboc/components/Vote/index.tsx
@@ -1,16 +1,32 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState, useMemo } from 'react'
 import './index.scss'
 import dayjs from 'dayjs'
-import { Tooltip } from 'antd'
+import QuestionHelper from '../../../../components/QuestionHelper'
+import NumericalInput from '../../../../components/NumericalInput'
+import { ButtonPrimary } from '../../../../components/Button'
+import { useActiveWeb3React } from '../../../../hooks'
+import { useWalletModalToggle } from '../../../../state/application/hooks'
+import { useTokenBalance } from '../../../../state/wallet/hooks'
+import { VELT } from '../../../../constants'
+import { Select } from 'antd'
+import { JSBI } from '@uniswap/sdk'
 
 interface VoteProps {
   votiingData: any
+  gombocList: any
 }
 
-const Vote = ({ votiingData }: VoteProps) => {
+const Vote = ({ votiingData, gombocList }: VoteProps) => {
+  const { account, chainId } = useActiveWeb3React()
+  const toggleWalletModal = useWalletModalToggle()
+  const veLtBal = useTokenBalance(account ?? undefined, VELT[chainId ?? 1])
+  const [voteAmount, setVoteAmount] = useState('')
+  const { Option } = Select
   const endDate = dayjs()
     .add(10, 'day')
     .format('YYYY-MM-DD')
+  const [amount, setAmount] = useState('')
+  const [curGomAddress, setCurGomAddress] = useState('')
   const [endTimeData, setEndTimeData] = useState({
     d: '00',
     h: '00',
@@ -49,6 +65,22 @@ const Vote = ({ votiingData }: VoteProps) => {
     }, 1000)
   }
 
+  const selList = useMemo(() => {
+    if (gombocList) {
+      const arr: any = []
+      gombocList.forEach((e: any) => {
+        const item = {
+          label: e.name,
+          value: e.gomboc
+        }
+        arr.push(item)
+      })
+      return arr
+    } else {
+      return []
+    }
+  }, [gombocList])
+
   useEffect((): any => {
     cd.current = votiingData.votingEndSeconds
     dealData()
@@ -57,6 +89,22 @@ const Vote = ({ votiingData }: VoteProps) => {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [votiingData])
+
+  function changeAmount(val: any) {
+    setAmount(val)
+    if (val && veLtBal && Number(veLtBal.toFixed(2)) > 0) {
+      const rate = Math.floor(Number(val) * 100)
+      const bal = veLtBal.multiply(JSBI.BigInt(rate)).divide(JSBI.BigInt(100))
+      setVoteAmount(bal?.toFixed(2))
+    } else {
+      setVoteAmount('')
+    }
+  }
+
+  function changeSel(val: string) {
+    setCurGomAddress(val)
+  }
+
   return (
     <div className="gom-vote-box">
       <h3 className="font-bolder text-white font-20">Proposed Gömböc Weight Changes</h3>
@@ -71,12 +119,10 @@ const Vote = ({ votiingData }: VoteProps) => {
         - Votes are time locked for 10 days. If you vote now, no edits can be made until{' '}
         <span className="text-primary">{endDate}</span>.
       </p>
-      <p className="text-center text-normal m-t-20">
+      <div className="text-center text-normal m-t-20 flex jc-center ai-center">
         Voting period ends
-        <Tooltip title="tip mes">
-          <i className="iconfont text-normal m-l-8 cursor-select">&#xe613;</i>
-        </Tooltip>
-      </p>
+        <QuestionHelper text="Voting period ends" />
+      </div>
       <div className="end-time-box flex m-t-20 w-100">
         <div className="flex-1">
           <p className="text-center text-gray">Day</p>
@@ -103,7 +149,57 @@ const Vote = ({ votiingData }: VoteProps) => {
           </div>
         </div>
       </div>
-      <div className="form-box"></div>
+      <div className="form-box m-t-20">
+        <p className="text-normal">Select a Gömböc </p>
+        <Select
+          value={curGomAddress}
+          onChange={(val: string) => {
+            changeSel(val)
+          }}
+          className="hp-select m-t-10"
+        >
+          {selList.map((data: any, index: number) => {
+            return (
+              <Option key={index} value={data.value}>
+                {data.label}
+              </Option>
+            )
+          })}
+        </Select>
+        <div className="flex jc-between m-t-30 m-b-10">
+          <span className="text-normal">Vote weight:</span>
+          <p>
+            unallocated votes : 80.88%
+            <span className="text-primary m-l-5">Lock</span>
+          </p>
+        </div>
+        <div className="hp-amount-box">
+          <NumericalInput
+            className={['hp-amount'].join(' ')}
+            value={amount}
+            decimals={2}
+            align={'right'}
+            onUserInput={val => {
+              changeAmount(val)
+            }}
+          />
+          <span className="input-tip">% of your voting power</span>
+        </div>
+        <p className="text-normal m-t-10">
+          {voteAmount || '--'} of your voting power will be allocated to this gömböc.
+        </p>
+        <div className="action-box m-t-40">
+          {!account ? (
+            <ButtonPrimary className="hp-button-primary" onClick={toggleWalletModal}>
+              Connect Wallet
+            </ButtonPrimary>
+          ) : (
+            <ButtonPrimary className="hp-button-primary" onClick={toggleWalletModal}>
+              Confirm Vote
+            </ButtonPrimary>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
