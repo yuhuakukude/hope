e54c097e42fde99f42db21136525f4b378bb7e2b
yuhuakukude <15577334054@163.com>
2023-02-08 19:54:23 +0800

    Set up staking tranction modal

--- src/pages/Staking/index.tsx
+++ src/pages/Staking/index.tsx
@@ -177,6 +177,7 @@ export default function Staking() {
           })
       })
       .catch(error => {
+        setStakePendingText('')
         onTxError(error)
       })
   }, [account, inputAmount, library, chainId, onTxStart, toStaked, onTxSubmitted, onTxError])
