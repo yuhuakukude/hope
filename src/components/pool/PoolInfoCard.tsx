f13f668f6edad6e31a854649b5ad4ce8539bb678
yuhuakukude <yuhuakukude@gmail.com>
2023-02-25 00:45:31 +0800

    add token price to Pool detail

--- src/components/pool/PoolInfoCard.tsx
+++ src/components/pool/PoolInfoCard.tsx
@@ -17,12 +17,25 @@ export const CardHeader = styled(AutoColumn)`
 export default function BasePoolInfoCard({ pool }: { pool?: PoolInfo }) {
   const { token1Deposited, token0Deposited, balance } = usePosition(pool?.pair)
   const { token0Staked, token1Staked, stakedLpAmount } = useStakePosition(pool)
-  console.log('tag', [pool?.tokens[0].address ?? '', pool?.tokens[1].address ?? ''])
   const tokenAddresses = useMemo(() => {
     return [pool?.tokens[0].address ?? '', pool?.tokens[1].address ?? '']
   }, [pool])
   const tokensPrice = useTokenPrice(tokenAddresses)
-  console.log('tokensPrice', tokensPrice)
+  const [token0Price, token1Price] = useMemo(() => {
+    return [
+      pool && tokensPrice.result
+        ? tokensPrice.result.find(token => {
+            return token.address.toLowerCase() === pool.tokens[0].address.toLowerCase()
+          })?.price
+        : undefined,
+      pool && tokensPrice.result
+        ? tokensPrice.result.find(token => {
+            return token.address.toLowerCase() === pool.tokens[1].address.toLowerCase()
+          })?.price
+        : undefined
+    ]
+  }, [pool, tokensPrice])
+
   return (
     <AutoColumn gap={'30px'} style={{ padding: '30px  20px' }}>
       <RowBetween>
@@ -41,31 +54,38 @@ export default function BasePoolInfoCard({ pool }: { pool?: PoolInfo }) {
             <CurrencyLogo size={'30px'} currency={pool?.tokens[0]} />
             <CurrencyLogo style={{ marginLeft: -15 }} size={'30px'} currency={pool?.tokens[1]} />
           </RowFixed>
-          <TYPE.white fontSize={18}>{`${pool?.tokens[0].symbol}-${pool?.tokens[1].symbol} Pool Token`}</TYPE.white>
+          <TYPE.white fontSize={18}>{`${pool?.tokens[0].symbol ?? ''}-${pool?.tokens[1].symbol ??
+            '-'} Pool Token`}</TYPE.white>
         </AutoRow>
         <TYPE.white fontSize={18}>{balance ? balance.toFixed(2, { groupSeparator: ',' }) : '--'}</TYPE.white>
       </RowBetween>
       <RowBetween>
-        <AutoRow gap={'10px'}>
+        <AutoRow gap={'5px'}>
           <RowFixed>
             <CurrencyLogo size={'20px'} currency={pool?.tokens[0]} />
           </RowFixed>
-          <TYPE.white fontSize={18}>{pool?.tokens[0].symbol}</TYPE.white>
+          <RowFixed>
+            <TYPE.white>{token0Deposited ? token0Deposited.toFixed(2) : '--'}</TYPE.white>
+            <TYPE.white>&ensp;{pool?.tokens[0].symbol}</TYPE.white>
+          </RowFixed>
         </AutoRow>
-        <TYPE.white fontSize={18}>
-          {token0Deposited ? token0Deposited.toFixed(2, { groupSeparator: ',' }) : '--'}
-        </TYPE.white>
+        <TYPE.main>
+          {token0Deposited && token0Price ? (Number(token0Deposited.toExact()) * Number(token0Price)).toFixed(2) : '--'}
+        </TYPE.main>
       </RowBetween>
       <RowBetween>
-        <AutoRow gap={'10px'}>
+        <AutoRow gap={'5px'}>
           <RowFixed>
             <CurrencyLogo size={'20px'} currency={pool?.tokens[1]} />
           </RowFixed>
-          <TYPE.white fontSize={18}>{pool?.tokens[1].symbol}</TYPE.white>
+          <RowFixed>
+            <TYPE.white>{token1Deposited ? token1Deposited.toFixed(2) : '--'}</TYPE.white>
+            <TYPE.white>&ensp;{pool?.tokens[1].symbol}</TYPE.white>
+          </RowFixed>
         </AutoRow>
-        <TYPE.white fontSize={18}>
-          {token1Deposited ? token1Deposited.toFixed(2, { groupSeparator: ',' }) : '--'}
-        </TYPE.white>
+        <TYPE.main>
+          {token1Deposited && token1Price ? (Number(token1Deposited.toExact()) * Number(token1Price)).toFixed(2) : '--'}
+        </TYPE.main>
       </RowBetween>
       <RowBetween>
         <TYPE.white fontWeight={700} fontSize={18}>
@@ -85,27 +105,35 @@ export default function BasePoolInfoCard({ pool }: { pool?: PoolInfo }) {
           </RowFixed>
           <TYPE.white fontSize={18}>{`${pool?.tokens[0].symbol}-${pool?.tokens[1].symbol} Pool Token`}</TYPE.white>
         </AutoRow>
-        <TYPE.white fontSize={18}>
-          {stakedLpAmount ? stakedLpAmount.toFixed(2, { groupSeparator: ',' }) : '--'}
-        </TYPE.white>
+        <TYPE.white>{stakedLpAmount ? stakedLpAmount.toFixed(2, { groupSeparator: ',' }) : '--'}</TYPE.white>
       </RowBetween>
       <RowBetween>
         <AutoRow gap={'10px'}>
           <RowFixed>
             <CurrencyLogo size={'20px'} currency={pool?.tokens[0]} />
           </RowFixed>
-          <TYPE.white fontSize={18}>{pool?.tokens[0].symbol}</TYPE.white>
+          <RowFixed>
+            <TYPE.white>{token0Staked ? token0Staked.toFixed(2) : '--'}</TYPE.white>
+            <TYPE.white>&ensp;{pool?.tokens[0].symbol}</TYPE.white>
+          </RowFixed>
         </AutoRow>
-        <TYPE.white fontSize={18}>{token0Staked ? token0Staked.toFixed(2, { groupSeparator: ',' }) : '--'}</TYPE.white>
+        <TYPE.main>
+          {token0Staked && token0Price ? (Number(token0Staked.toExact()) * Number(token0Price)).toFixed(2) : '--'}
+        </TYPE.main>
       </RowBetween>
       <RowBetween>
         <AutoRow gap={'10px'}>
           <RowFixed>
             <CurrencyLogo size={'20px'} currency={pool?.tokens[1]} />
           </RowFixed>
-          <TYPE.white fontSize={18}>{pool?.tokens[1].symbol}</TYPE.white>
+          <RowFixed>
+            <TYPE.white>{token1Staked ? token1Staked.toFixed(2) : '--'}</TYPE.white>
+            <TYPE.white>&ensp;{pool?.tokens[1].symbol}</TYPE.white>
+          </RowFixed>
         </AutoRow>
-        <TYPE.white fontSize={18}>{token1Staked ? token1Staked.toFixed(2, { groupSeparator: ',' }) : '--'}</TYPE.white>
+        <TYPE.main>
+          {token1Staked && token1Staked ? (Number(token1Staked.toExact()) * Number(token1Price)).toFixed(2) : '--'}
+        </TYPE.main>
       </RowBetween>
     </AutoColumn>
   )
