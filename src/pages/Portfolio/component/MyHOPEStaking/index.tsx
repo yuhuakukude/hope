b772dcae95bd06de55096df87e5a90241c78f2b9
kangbaohu <abudaoshi@gmail.com>
2023-03-02 00:54:24 +0800

    my hope staking optimize

--- src/pages/Portfolio/component/MyHOPEStaking/index.tsx
+++ src/pages/Portfolio/component/MyHOPEStaking/index.tsx
@@ -1,10 +1,9 @@
 import Table from 'components/antd/Table'
-import { STAKING_HOPE_GOMBOC_ADDRESS, ST_HOPE } from '../../../../constants/index'
 import { useActiveWeb3React } from 'hooks'
 import { toUsdPrice } from 'hooks/ahp/usePortfolio'
 import { useStaking } from 'hooks/ahp/useStaking'
 import usePrice from 'hooks/usePrice'
-import React from 'react'
+import React, { useCallback, useMemo, useState } from 'react'
 import { useTokenBalance } from 'state/wallet/hooks'
 import Card from '../Card'
 import Item from '../Item'
@@ -12,82 +11,14 @@ import SelectTips, { TitleTipsProps } from '../SelectTips'
 import { usePoolGomContract } from 'hooks/useContract'
 import { useSingleCallResult } from 'state/multicall/hooks'
 import { CurrencyAmount, TokenAmount } from '@uniswap/sdk'
-
-const columns = [
-  {
-    title: 'Protocol',
-    dataIndex: 'Protocol',
-    key: 'Protocol',
-    render: () => 'HOPE Staking'
-  },
-  {
-    title: 'My Boost',
-    dataIndex: 'boost',
-    key: 'boost'
-  },
-  {
-    title: 'Balance',
-    dataIndex: 'stHOPE',
-    key: 'stHOPE',
-    render: (text: string, record: IStaking) => {
-      return <Item title={record.stHOPE} desc={record.usdOfStHOPE} />
-    }
-  },
-  {
-    title: 'Unstaking',
-    dataIndex: 'unstaking',
-    key: 'unstaking',
-    render: (text: string, record: IStaking) => {
-      return <Item title={record.unstaking} desc={record.usdOfUnstaking} />
-    }
-  },
-  {
-    title: 'Unstaked',
-    dataIndex: 'unstaked',
-    key: 'unstaked',
-    render: (text: string, record: IStaking) => {
-      return <Item title={record.unstaked} desc={record.usdOfUnstaked} />
-    }
-  },
-  {
-    title: 'Claimable Rewards',
-    dataIndex: 'claRewards',
-    key: 'claRewards',
-    render: (text: string, record: IStaking) => {
-      return <Item title={record.claRewards} desc={record.usdOfClaRewards} />
-    }
-  },
-  {
-    title: 'Actions',
-    dataIndex: 'actions',
-    key: 'actions',
-    render: () => {
-      const options: TitleTipsProps[] = [
-        {
-          label: 'Stake',
-          value: 'Stake',
-          onClick: () => {}
-        },
-        {
-          label: 'Unstake',
-          value: 'Unstake',
-          onClick: () => {}
-        },
-        {
-          label: 'Claim Rewards',
-          value: 'Claim Rewards',
-          onClick: () => {}
-        },
-        {
-          label: 'Yield Boost',
-          value: 'Yield Boost',
-          onClick: () => {}
-        }
-      ]
-      return <SelectTips options={options} />
-    }
-  }
-]
+import TransactionConfirmationModal, {
+  TransactionErrorContent
+} from '../../../../components/TransactionConfirmationModal'
+import { useHistory } from 'react-router-dom'
+import { Token } from '@uniswap/sdk'
+import GombocClaim from '../../../../components/ahp/GombocClaim'
+import { useToClaim, useClaimRewards } from '../../../../hooks/ahp/usePortfolio'
+import { STAKING_HOPE_GOMBOC_ADDRESS, LT, HOPE, ST_HOPE } from '../../../../constants'
 
 interface IStaking {
   stHOPE: string
@@ -142,9 +73,212 @@ export default function MyHOPEStaking() {
     usdOfUnstaked: getUsdPrice(hopePrice, unstakedVal)
   }
 
+  const history = useHistory()
+  const [curTableItem, setCurTableItem]: any = useState({})
+  const [txHash, setTxHash] = useState<string>('')
+  const [errorStatus, setErrorStatus] = useState<{ code: number; message: string } | undefined>()
+  const [attemptingTxn, setAttemptingTxn] = useState(false) // clicked confirm
+  const [showConfirm, setShowConfirm] = useState<boolean>(false)
+  const [curToken, setCurToken] = useState<Token | undefined>(HOPE[chainId ?? 1])
+
+  function ClaimFn(item: any) {
+    setCurTableItem(item)
+    setTxHash('')
+    setErrorStatus(undefined)
+    setAttemptingTxn(false)
+    setShowConfirm(true)
+  }
+
+  const onTxStart = useCallback(() => {
+    setShowConfirm(true)
+    setAttemptingTxn(true)
+  }, [])
+
+  const onTxSubmitted = useCallback((hash: string | undefined) => {
+    setShowConfirm(true)
+    setAttemptingTxn(false)
+    hash && setTxHash(hash)
+  }, [])
+
+  const onTxError = useCallback(error => {
+    setShowConfirm(true)
+    setTxHash('')
+    setAttemptingTxn(false)
+    setErrorStatus({ code: error?.code, message: error.message })
+  }, [])
+
+  const [claimPendingText, setPendingText] = useState('')
+  const { toClaim } = useToClaim()
+
+  const claimCallback = useCallback(async () => {
+    if (!account) return
+    setCurToken(LT[chainId ?? 1])
+    onTxStart()
+    setPendingText(`claim Rewards`)
+    toClaim(STAKING_HOPE_GOMBOC_ADDRESS[chainId ?? 1])
+      .then(hash => {
+        setPendingText('')
+        onTxSubmitted(hash)
+      })
+      .catch((error: any) => {
+        setPendingText('')
+        onTxError(error)
+      })
+  }, [account, chainId, onTxError, onTxStart, onTxSubmitted, toClaim])
+
+  const curAddress = useMemo(() => {
+    let res = ''
+    if (curTableItem && curTableItem?.gomboc) {
+      res = curTableItem?.gomboc
+    }
+    return res
+  }, [curTableItem])
+  const { toClaimRewards } = useClaimRewards(curAddress)
+  const claimRewardsCallback = useCallback(async () => {
+    if (!account) return
+    setCurToken(LT[chainId ?? 1])
+    onTxStart()
+    setPendingText(`claim Rewards`)
+    toClaimRewards()
+      .then(hash => {
+        setPendingText('')
+        onTxSubmitted(hash)
+      })
+      .catch((error: any) => {
+        setPendingText('')
+        onTxError(error)
+      })
+  }, [account, chainId, onTxError, onTxStart, onTxSubmitted, toClaimRewards])
+
+  const claimSubmit = useCallback(
+    (type: string) => {
+      if (type === 'normal') {
+        claimCallback()
+      } else {
+        claimRewardsCallback()
+      }
+    },
+    [claimCallback, claimRewardsCallback]
+  )
+
+  const confirmationContent = useCallback(
+    () =>
+      errorStatus ? (
+        <TransactionErrorContent
+          errorCode={errorStatus.code}
+          onDismiss={() => setShowConfirm(false)}
+          message={errorStatus.message}
+        />
+      ) : (
+        <GombocClaim
+          onSubmit={(type: string) => {
+            claimSubmit(type)
+          }}
+          onDismiss={() => setShowConfirm(false)}
+          tableItem={curTableItem}
+        />
+      ),
+    [claimSubmit, errorStatus, curTableItem]
+  )
+
+  const columns = [
+    {
+      title: 'Protocol',
+      dataIndex: 'Protocol',
+      key: 'Protocol',
+      render: () => 'HOPE Staking'
+    },
+    {
+      title: 'My Boost',
+      dataIndex: 'boost',
+      key: 'boost'
+    },
+    {
+      title: 'Balance',
+      dataIndex: 'stHOPE',
+      key: 'stHOPE',
+      render: (text: string, record: IStaking) => {
+        return <Item title={record.stHOPE} desc={record.usdOfStHOPE} />
+      }
+    },
+    {
+      title: 'Unstaking',
+      dataIndex: 'unstaking',
+      key: 'unstaking',
+      render: (text: string, record: IStaking) => {
+        return <Item title={record.unstaking} desc={record.usdOfUnstaking} />
+      }
+    },
+    {
+      title: 'Unstaked',
+      dataIndex: 'unstaked',
+      key: 'unstaked',
+      render: (text: string, record: IStaking) => {
+        return <Item title={record.unstaked} desc={record.usdOfUnstaked} />
+      }
+    },
+    {
+      title: 'Claimable Rewards',
+      dataIndex: 'claRewards',
+      key: 'claRewards',
+      render: (text: string, record: IStaking) => {
+        return <Item title={record.claRewards} desc={record.usdOfClaRewards} />
+      }
+    },
+    {
+      title: 'Actions',
+      dataIndex: 'actions',
+      key: 'actions',
+      render: (text: string, record: IStaking) => {
+        const options: TitleTipsProps[] = [
+          {
+            label: 'Stake',
+            value: 'Stake',
+            onClick: () => {
+              history.push(`/staking`)
+            }
+          },
+          {
+            label: 'Unstake',
+            value: 'Unstake',
+            onClick: () => {
+              history.push(`/staking`)
+            }
+          },
+          {
+            label: 'Claim Rewards',
+            value: 'Claim Rewards',
+            onClick: () => {
+              ClaimFn(record)
+            }
+          },
+          {
+            label: 'Yield Boost',
+            value: 'Yield Boost',
+            onClick: () => {
+              history.push(`/dao/gomboc?gomboc=${curAddress}`) // TODO Sure
+            }
+          }
+        ]
+        return <SelectTips options={options} />
+      }
+    }
+  ]
+
   return (
-    <Card title="My HOPE Staking">
-      <Table columns={columns} dataSource={[data]} pagination={false}></Table>
-    </Card>
+    <>
+      <TransactionConfirmationModal
+        isOpen={showConfirm}
+        onDismiss={() => setShowConfirm(false)}
+        attemptingTxn={attemptingTxn}
+        hash={txHash}
+        content={confirmationContent}
+        pendingText={claimPendingText}
+        currencyToAdd={curToken}
+      />
+      <Card title="My HOPE Staking">
+        <Table columns={columns} dataSource={[data]} pagination={false}></Table>
+      </Card>
+    </>
   )
 }
--- src/pages/Portfolio/component/MyLiquidityPools/index.tsx
+++ src/pages/Portfolio/component/MyLiquidityPools/index.tsx
@@ -1,10 +1,11 @@
 import PortfolioApi, { ILiquidityPools } from 'api/portfolio.api'
 import Table from 'components/antd/Table'
+
 import { useActiveWeb3React } from 'hooks'
 import React, { useEffect, useState } from 'react'
 import Card from '../Card'
 import Item from '../Item'
-import SelectTips, { TitleTipsProps } from '../SelectTips'
+// import SelectTips, { TitleTipsProps } from '../SelectTips'
 
 function toFixed(val: string | number, length: number = 2) {
   if (isNaN(val as number)) {
@@ -13,117 +14,6 @@ function toFixed(val: string | number, length: number = 2) {
   return Number(val).toFixed(length)
 }
 
-const columns = [
-  {
-    title: 'Pools',
-    dataIndex: 'composition',
-    key: 'composition',
-    render: (text: string, record: ILiquidityPools) => {
-      return (
-        <Item
-          title={
-            <>
-              <span>
-                <i className="iconfont"></i>
-                <i className="iconfont"></i>
-              </span>
-              <span>{record.composition}</span>
-            </>
-          }
-          desc={<>Fee Rate: {record.feeRate}%</>}
-        />
-      )
-    }
-  },
-  {
-    title: 'My Composition',
-    dataIndex: 'token0Balance',
-    key: 'token0Balance',
-    render: (text: string, record: ILiquidityPools) => {
-      return (
-        <Item
-          type={2}
-          title={
-            <>
-              <i className="iconfont"></i>
-              {toFixed(record.token0Balance, 8)}
-              &nbsp;USDC
-            </>
-          }
-          desc={
-            <>
-              <i className="iconfont"></i>
-              {toFixed(record.token1Balance, 8)}
-              &nbsp;BUSD
-            </>
-          }
-        />
-      )
-    }
-  },
-  {
-    title: 'LP Tokens',
-    dataIndex: 'lpBalance',
-    key: 'lpBalance',
-    render: (text: string, record: ILiquidityPools) => {
-      return <Item title={toFixed(record.lpBalance)} desc={'≈ $' + toFixed(record.hopeOfLpBalance)} />
-    }
-  },
-  {
-    title: 'Staked LP Tokens',
-    dataIndex: 'stakedLpBalance',
-    key: 'stakedLpBalance',
-    render: (text: string, record: ILiquidityPools) => {
-      return <Item title={toFixed(record.stakedLpBalance)} desc={'≈ $' + toFixed(record.hopeOfStakedLpBalance)} />
-    }
-  },
-  {
-    title: 'APR',
-    dataIndex: 'feesApr',
-    key: 'feesApr',
-    render: (text: string, record: ILiquidityPools) => {
-      return <Item type={3} title={`${record.feesApr || 0}%`} desc={`${record.ltApr}% -> ${record.maxLtApr}%`} />
-    }
-  },
-  {
-    title: 'Claimable Rewards',
-    dataIndex: 'ltOfReward',
-    key: 'ltOfReward',
-    render: (text: string, record: ILiquidityPools) => {
-      return <Item title={toFixed(record.ltOfReward)} desc={'≈ $' + toFixed(record.ltTotalReward)} />
-    }
-  },
-  {
-    title: 'Actions',
-    dataIndex: 'actions',
-    key: 'actions',
-    render: () => {
-      const options: TitleTipsProps[] = [
-        {
-          label: 'Stake',
-          value: 'Stake',
-          onClick: () => {}
-        },
-        {
-          label: 'Unstake',
-          value: 'Unstake',
-          onClick: () => {}
-        },
-        {
-          label: 'Claim Rewards',
-          value: 'Claim Rewards',
-          onClick: () => {}
-        },
-        {
-          label: 'Yield Boost',
-          value: 'Yield Boost',
-          onClick: () => {}
-        }
-      ]
-      return <SelectTips options={options} />
-    }
-  }
-]
 export default function MyLiquidityPools() {
   const { account } = useActiveWeb3React()
   const [dataSource, setDataSource] = useState<ILiquidityPools[]>([])
@@ -138,9 +28,131 @@ export default function MyLiquidityPools() {
       }
     })
   }, [account])
+
+  const columns = [
+    {
+      title: 'Pools',
+      dataIndex: 'composition',
+      key: 'composition',
+      render: (text: string, record: ILiquidityPools) => {
+        return (
+          <Item
+            title={
+              <>
+                <span>
+                  <i className="iconfont"></i>
+                  <i className="iconfont"></i>
+                </span>
+                <span>{record.composition}</span>
+              </>
+            }
+            desc={<>Fee Rate: {record.feeRate}%</>}
+          />
+        )
+      }
+    },
+    {
+      title: 'My Composition',
+      dataIndex: 'token0Balance',
+      key: 'token0Balance',
+      render: (text: string, record: ILiquidityPools) => {
+        return (
+          <Item
+            type={2}
+            title={
+              <>
+                <i className="iconfont"></i>
+                {toFixed(record.token0Balance, 8)}
+                &nbsp;USDC
+              </>
+            }
+            desc={
+              <>
+                <i className="iconfont"></i>
+                {toFixed(record.token1Balance, 8)}
+                &nbsp;BUSD
+              </>
+            }
+          />
+        )
+      }
+    },
+    {
+      title: 'LP Tokens',
+      dataIndex: 'lpBalance',
+      key: 'lpBalance',
+      render: (text: string, record: ILiquidityPools) => {
+        return <Item title={toFixed(record.lpBalance)} desc={'≈ $' + toFixed(record.hopeOfLpBalance)} />
+      }
+    },
+    {
+      title: 'Staked LP Tokens',
+      dataIndex: 'stakedLpBalance',
+      key: 'stakedLpBalance',
+      render: (text: string, record: ILiquidityPools) => {
+        return <Item title={toFixed(record.stakedLpBalance)} desc={'≈ $' + toFixed(record.hopeOfStakedLpBalance)} />
+      }
+    },
+    {
+      title: 'APR',
+      dataIndex: 'feesApr',
+      key: 'feesApr',
+      render: (text: string, record: ILiquidityPools) => {
+        return <Item type={3} title={`${record.feesApr || 0}%`} desc={`${record.ltApr}% -> ${record.maxLtApr}%`} />
+      }
+    },
+    {
+      title: 'Claimable Rewards',
+      dataIndex: 'ltOfReward',
+      key: 'ltOfReward',
+      render: (text: string, record: ILiquidityPools) => {
+        return <Item title={toFixed(record.ltOfReward)} desc={'≈ $' + toFixed(record.ltTotalReward)} />
+      }
+    },
+    {
+      title: 'Actions',
+      dataIndex: 'actions',
+      key: 'actions'
+      // render: (text: string, record: ILiquidityPools) => {
+      //   const options: TitleTipsProps[] = [
+      //     {
+      //       label: 'Stake',
+      //       value: 'Stake',
+      //       onClick: () => {
+      //         history.push(`/staking`)
+      //       }
+      //     },
+      //     {
+      //       label: 'Unstake',
+      //       value: 'Unstake',
+      //       onClick: () => {
+      //         history.push(`/staking`)
+      //       }
+      //     },
+      //     {
+      //       label: 'Claim Rewards',
+      //       value: 'Claim Rewards',
+      //       onClick: () => {
+      //         ClaimFn(record)
+      //       }
+      //     },
+      //     {
+      //       label: 'Yield Boost',
+      //       value: 'Yield Boost',
+      //       onClick: () => {
+      //         history.push(`/dao/gomboc?gomboc=${record.gomboc}`)
+      //       }
+      //     }
+      //   ]
+      //   return <SelectTips options={options} />
+      // }
+    }
+  ]
   return (
-    <Card title="My Liquidity Pools">
-      <Table columns={columns} dataSource={dataSource}></Table>
-    </Card>
+    <>
+      <Card title="My Liquidity Pools">
+        <Table columns={columns} dataSource={dataSource}></Table>
+      </Card>
+    </>
   )
 }
