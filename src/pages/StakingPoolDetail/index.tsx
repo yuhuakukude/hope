3b2640fcfba34936fdf4f7ef51154138d5c8c618
hyc <hyc@babelbank.io>
2023-03-09 19:30:19 +0800

    fix charts

--- src/pages/StakingPoolDetail/index.tsx
+++ src/pages/StakingPoolDetail/index.tsx
@@ -704,7 +704,7 @@ export default function StakingPoolDetail({
                 <Row>
                   <TYPE.body>APR</TYPE.body>
                   <TYPE.green fontSize={30} marginLeft={12} fontFamily={'Arboria-Medium'}>
-                    {format.rate(aprInfo.baseApr)}
+                    {amountFormat(aprInfo.baseApr, 2)}%
                   </TYPE.green>
                 </Row>
                 <p className="m-t-15 text-normal">Fees APR: {amountFormat(aprInfo.feeApr * 100, 2)}% </p>
