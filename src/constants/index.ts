4a0c7c3da2e4bf2ba8bfeacc6f22472bf59ca5f6
koufeng <feng.kou@hfin.io>
2023-03-13 22:01:26 +0800

    url change

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { injected, walletconnect } from '../connectors'
 
-export const SUBGRAPH = 'https://hope-dapp-dev1.hivefin.net/subgraphs/name/light-test'
+export const SUBGRAPH = 'https://hope-dapp-dev1.hivefin.net/subgraphs/name/light-beta'
 export const DOC_API = 'https://hope-static-test1.hivefin.net'
 
 export const BLOCK_SUBGRAPH = 'https://hope-dapp-dev1.hivefin.net/subgraphs/name/light-dev/ethereum-blocks'
