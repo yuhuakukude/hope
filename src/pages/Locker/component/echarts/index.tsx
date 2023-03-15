d5b6a78e69228d041266d8fe0f436d228c22d3b1
hyc <hyc@babelbank.io>
2023-03-15 21:02:55 +0800

    fix tick

--- src/pages/Locker/component/echarts/index.tsx
+++ src/pages/Locker/component/echarts/index.tsx
@@ -106,7 +106,7 @@ export default function LockerEcharts() {
                 }
               },
               axisTick: {
-                show: false
+                alignWithLabel: true
               }
             },
             yAxis: {
