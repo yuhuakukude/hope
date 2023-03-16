f8a539f897719756ae40d2884ad566afde3ebd6a
hyc <hyc@babelbank.io>
2023-03-16 19:43:02 +0800

    fix locker goc banner

--- src/pages/Locker/component/Banner/index.tsx
+++ src/pages/Locker/component/Banner/index.tsx
@@ -6,23 +6,25 @@ export default function LockerBanner() {
   return (
     <div className="locker-banner-box">
       <div className="banner p-30">
-        <h2 className="text-medium">Lock your LT to acquire veLT</h2>
-        <p className="font-nor m-t-20 flex ai-center">
-          Extra earnings & voting power{' '}
-          <a
-            href={DOCS_URL['LightToken']}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="link text-primary m-l-20 flex ai-center"
-          >
-            Learn more <i className="iconfont m-l-5 m-t-2">&#xe619;</i>{' '}
-          </a>
-        </p>
-        <ul className="m-t-20">
-          <li className="font-nor">- Boost liquidity mining yield up to 2.5x</li>
-          <li className="font-nor">- Vote to direct liquidity mining emissions</li>
-          <li className="font-nor">- Earn your share of protocol revenue</li>
-        </ul>
+        <div className="content">
+          <h2 className="text-medium">Lock your LT to acquire veLT</h2>
+          <p className="font-nor m-t-20 flex ai-center">
+            Extra earnings & voting power{' '}
+            <a
+              href={DOCS_URL['LightToken']}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link text-primary m-l-20 flex ai-center"
+            >
+              Learn more <i className="iconfont m-l-5 m-t-2">&#xe619;</i>{' '}
+            </a>
+          </p>
+          <ul className="m-t-20">
+            <li className="font-nor">- Boost liquidity mining yield up to 2.5x</li>
+            <li className="font-nor">- Vote to direct liquidity mining emissions</li>
+            <li className="font-nor">- Earn your share of protocol revenue</li>
+          </ul>
+        </div>
       </div>
     </div>
   )
