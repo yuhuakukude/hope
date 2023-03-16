276d0bc036ffbe30b38be6189ced9230b3505862
hyc <hyc@babelbank.io>
2023-03-16 20:33:57 +0800

    fix query catch error

--- src/utils/graph.ts
+++ src/utils/graph.ts
@@ -1,14 +1,19 @@
 export const postQuery = async (endpoint: string, query: string, variables?: {}) => {
-  const options = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ query, variables })
-  }
-  const response = await fetch(endpoint, options)
-  const data = await response.json()
-  if (data.errors) {
-    throw new Error(data.errors[0].message)
-  } else {
-    return data
+  try {
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ query, variables })
+    }
+    const response = await fetch(endpoint, options)
+    const data = await response.json()
+    if (data.errors) {
+      throw new Error(data.errors[0].message)
+    } else {
+      return data
+    }
+  } catch (error) {
+    console.log(error)
+    return undefined
   }
 }
