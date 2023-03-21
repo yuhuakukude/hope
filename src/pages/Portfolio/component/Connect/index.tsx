e204877bf8af9e1ee55799e0ddc0c018e4ecf800
hyc <hyc@babelbank.io>
2023-03-21 15:50:59 +0800

    fix img

--- src/pages/Portfolio/component/Connect/index.tsx
+++ src/pages/Portfolio/component/Connect/index.tsx
@@ -1,23 +1,20 @@
 import { ButtonPrimary } from 'components/Button'
 import React from 'react'
 import { useWalletModalToggle } from 'state/application/hooks'
-import Card from '../Card'
 import './index.scss'
 
 export default function PortfolioConnect() {
   const toggleWalletModal = useWalletModalToggle()
 
   return (
-    <div className="portfolio-connect">
-      <Card>
-        <div className="portfolio-connect-bg"></div>
-        <div className="portfolio-connect-title">Connect to a wallet to view your portfolio. </div>
-        <div className="portfolio-connect-button">
-          <ButtonPrimary className="portfolio-button" onClick={toggleWalletModal}>
-            Connect Wallet
-          </ButtonPrimary>
-        </div>
-      </Card>
+    <div className="portfolio-connect m-t-100">
+      <div className="portfolio-connect-bg"></div>
+      <div className="portfolio-connect-title">Connect to a wallet to view your portfolio. </div>
+      <div className="portfolio-connect-button">
+        <ButtonPrimary className="portfolio-button" onClick={toggleWalletModal}>
+          Connect Wallet
+        </ButtonPrimary>
+      </div>
     </div>
   )
 }
